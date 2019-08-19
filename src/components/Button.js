5d906d96677fe41b94debe8e480872cd59f88cd4
Kinark <igormarcossi@gmail.com>
2019-08-19 20:54:33 -0300

    Added Button component

---
+++ src/components/Button.js
@@ -0,0 +1,28 @@
+import styled, { css } from 'styled-components'
+import { Link } from 'gatsby'
+
+import colors from '../constants/colors'
+
+const ButtonBase = css`
+   background-color: ${colors.TITLE};
+   color: ${colors.BG};
+   border-radius: 100px;
+   padding: calc(0.75rem - 3px) 2rem 0.75rem;
+   display: inline-block;
+   margin: 1rem 0;
+   box-shadow: 0px 10px 15px 0px ${colors.SHADOW};
+   transition: color 250ms ease-out, background-color 250ms ease-out, box-shadow 250ms ease-out;
+   &:hover {
+      color: ${colors.TITLE};
+      background-color: ${colors.CARD_BG};
+      box-shadow: 0px 15px 20px 0px ${colors.SHADOW};
+   }
+`
+
+export const ButtonLink = styled(Link)`
+   ${ButtonBase}
+`
+
+export const ButtonAnchor = styled.a`
+   ${ButtonBase}
+`
