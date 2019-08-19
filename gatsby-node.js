c925427dff091903f03a0bdbe65509cce2b3ed54
Kinark <igormarcossi@gmail.com>
2019-08-19 15:38:25 -0300

    Removed tags from node query

--- gatsby-node.js
+++ gatsby-node.js
@@ -16,7 +16,6 @@ exports.createPages = ({ actions, graphql }) => {
                      slug
                   }
                   frontmatter {
-                     tags
                      templateKey
                   }
                }
