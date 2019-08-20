70df1c6d5f7ea20425d24ef969ad9c995dd562c0
Kinark <igormarcossi@gmail.com>
2019-08-19 23:41:53 -0300

    Created SmallTitle component

---
+++ src/components/SmallTitle.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+import colors from '../constants/colors'
+
+const SmallTitle = styled.h6`
+   font-size: 1.25rem;
+   color: ${colors.TITLE};
+   font-weight: 700;
+   margin: 0.75rem 0;
+`
+export default SmallTitle
