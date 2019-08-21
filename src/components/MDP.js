ccf6d5c8d37a95566436f1ecaef0c86365d93d2b
Kinark <igormarcossi@gmail.com>
2019-08-21 18:32:55 -0300

    Added rest props to MDP component

--- src/components/MDP.js
+++ src/components/MDP.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import styled from 'styled-components';
+import styled from 'styled-components'
 
-const MDP = ({children}) => <MDPWrapper dangerouslySetInnerHTML={{__html: children}} />
+const MDP = ({ children, ...rest }) => <MDPWrapper dangerouslySetInnerHTML={{ __html: children }} {...rest} />
 
 const MDPWrapper = styled.p`
    white-space: pre-line;
-`;
+`
 
 MDP.propTypes = {
    children: PropTypes.node.isRequired
