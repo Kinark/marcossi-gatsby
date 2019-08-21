b04485bf677a92844baea3326766e108ce393348
Kinark <igormarcossi@gmail.com>
2019-08-21 14:29:19 -0300

    Added some more styles to Button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -12,11 +12,18 @@ const ButtonBase = css`
    margin: 1rem 0;
    box-shadow: 0px 10px 15px 0px ${colors.SHADOW};
    transition: color 250ms ease-out, background-color 250ms ease-out, box-shadow 250ms ease-out;
+   cursor: pointer;
+   border: none;
+   font-family: inherit;
    &:hover {
       color: ${colors.TITLE};
       background-color: ${colors.CARD_BG};
       box-shadow: 0px 15px 20px 0px ${colors.SHADOW};
    }
+`
+
+export const Button = styled.button`
+   ${ButtonBase}
 `
 
 export const ButtonLink = styled(Link)`
