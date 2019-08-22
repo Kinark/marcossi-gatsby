4a5da249fbd3490ca8d5836bf752afd853e57d76
Kinark <igormarcossi@gmail.com>
2019-08-22 18:26:24 -0300

    Finished page 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,42 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import { graphql } from 'gatsby'
+import styled from 'styled-components'
+import notFound from '../img/notFound.png'
+import SubTitle from '../components/SubTitle'
 
-const NotFoundPage = () => (
-  <Layout>
-    <div>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </div>
-  </Layout>
+const NotFoundPage = ({ data }) => (
+   <Layout bgImg={data.markdownRemark.frontmatter.backgroundImage} logo={data.markdownRemark.frontmatter.logo}>
+      <Wrapper className="container">
+         <img width="250" src={notFound} alt="" />
+         <SubTitle>Page <span>not</span> found!</SubTitle>
+         <SubTitle>Página <span>não</span> encontrada!</SubTitle>
+      </Wrapper>
+   </Layout>
 )
 
 export default NotFoundPage
+
+const Wrapper = styled.div`
+   display: flex;
+   text-align: center;
+   flex-direction: column;
+   align-items: center;
+   height: calc(100vh - 5rem);
+   justify-content: center;
+`
+
+export const pageQuery = graphql`
+   query NotFoundQuery {
+      markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
+         frontmatter {
+            backgroundImage {
+               publicURL
+            }
+            logo {
+               publicURL
+            }
+         }
+      }
+   }
+`
