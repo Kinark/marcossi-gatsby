8950724bbe5450cca14a0adf0190ba63e2d1a8e7
Kinark <igormarcossi@gmail.com>
2019-09-18 18:13:53 -0300

    Fixed word-break on Title

--- src/components/Title.js
+++ src/components/Title.js
@@ -5,7 +5,8 @@ import colors from '../constants/colors'
 const Title = styled.h1`
    color: ${colors.TITLE};
    font-weight: 700;
-   font-size: 48px;
+   font-size: 3.2rem;
+   word-break: break-word;
    margin: 1rem 0;
 `
 
